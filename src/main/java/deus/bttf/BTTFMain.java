ca512248bd177fb6f55d7c248ba4b1e335d8a5ce
Garkatron <145294535+Garkatron@users.noreply.github.com>
2024-10-20 16:05:44 +0200

    1.0.3 Fixed spider eye name

--- src/main/java/deus/bttf/BTTFMain.java
+++ src/main/java/deus/bttf/BTTFMain.java
@@ -5,6 +5,7 @@ import deus.bttf.Config.ModConfig;
 import deus.bttf.Items.BTTFItems;
 import deus.bttf.Recipes.RecipeInitializer;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.loader.api.FabricLoader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import turniplabs.halplibe.util.GameStartEntrypoint;
@@ -23,8 +24,8 @@ public class BTTFMain implements ModInitializer, GameStartEntrypoint, RecipeEntr
 
 		isDebug = java.lang.management.ManagementFactory.getRuntimeMXBean().
 			getInputArguments().toString().contains("-agentlib:jdwp");
-
 		LOGGER.info("Fishing Additions initialized. Debug: "+ isDebug);
+
 
 		new BTTFItems().Initialize();
 
--- src/main/java/deus/bttf/mixin/FAFishingRodMixin.java
+++ src/main/java/deus/bttf/mixin/FAFishingRodMixin.java
@@ -7,14 +7,13 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
 
-@Mixin(EntityBobber.class)
+@Mixin(value = EntityBobber.class, priority = 1001)
 public class FAFishingRodMixin {
 
 	@ModifyArg(method = "yoink()I", at = @At(value = "INVOKE", target = "Lnet/minecraft/core/entity/EntityItem;<init>(Lnet/minecraft/core/world/World;DDDLnet/minecraft/core/item/ItemStack;)V"), index = 4,remap = false)
 	private ItemStack modifyItemStack(ItemStack originalStack) {
+
 		return CustomBobberEntity.getFishByProbability().getDefaultStack();
+
 	}
-
-
-
 }
